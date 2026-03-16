609a8f1bdbfb8b59f9defe1e3750763a0bf3bc9f
Gnatear <tzh26144@gmail.com>
2026-03-16 17:02:22 -0300

    basic functions of csv.sh done

--- scripts/csv.sh
+++ scripts/csv.sh
@@ -1,30 +1,52 @@
-csv_find_by_id(){
+filename="shell_text_dataset.csv"
 
+csv_find_by_id(){
+    while IFS= read -r line; do
+        awk -F',' '{print $1}' shell_text_dataset.csv
+    done
 }
 csv_find_by_name(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $2}' shell_text_dataset.csv
+    done
 }
 csv_find_by_email(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $3}' shell_text_dataset.csv
+    done
 }
 csv_find_by_city(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $4}' shell_text_dataset.csv
+    done
 }
 csv_find_by_number(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $5}' shell_text_dataset.csv
+    done
 }
 csv_find_by_unicode(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $6}' shell_text_dataset.csv
+    done
 }
 csv_find_by_sentence(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $7}' shell_text_dataset.csv
+    done
 }
 csv_find_by_code(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $8}' shell_text_dataset.csv
+    done
 }
 csv_find_by_url(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $9}' shell_text_dataset.csv
+    done
 }
 csv_find_by_tags(){
-
+    while IFS= read -r line; do
+        awk -F',' '{print $10}' shell_text_dataset.csv
+    done
 }
