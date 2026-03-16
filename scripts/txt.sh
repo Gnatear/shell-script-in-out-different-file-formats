27db9b4f7edb9bf17d82ebddc2098fa9d13f4491
Gnatear <tzh26144@gmail.com>
2026-03-16 17:18:21 -0300

    basic functions of txt.sh done

--- scripts/txt.sh
+++ scripts/txt.sh
@@ -1,30 +1,51 @@
-txt_find_by_id(){
-
+filename="shell_text_dataset.txt"
+md_find_by_id(){
+while IFS= read -r line; do
+        awk -F' ' '{print $1}' shell_text_dataset.txt
+    done
 }
-txt_find_by_name(){
-
+md_find_by_name(){
+while IFS= read -r line; do
+        awk -F' ' '{print $2}' shell_text_dataset.txt
+    done
 }
-txt_find_by_email(){
-
+md_find_by_email(){
+while IFS= read -r line; do
+        awk -F' ' '{print $3}' shell_text_dataset.txt
+    done
 }
-txt_find_by_city(){
-
+md_find_by_city(){
+while IFS= read -r line; do
+        awk -F' ' '{print $4}' shell_text_dataset.txt
+    done
 }
-txt_find_by_number(){
-
+md_find_by_number(){
+while IFS= read -r line; do
+        awk -F' ' '{print $5}' shell_text_dataset.txt
+    done
 }
-txt_find_by_unicode(){
-
+md_find_by_unicode(){
+while IFS= read -r line; do
+        awk -F' ' '{print $6}' shell_text_dataset.txt
+    done
 }
-txt_find_by_sentence(){
-
+md_find_by_sentence(){
+while IFS= read -r line; do
+        awk -F' ' '{print $7}' shell_text_dataset.txt
+    done
 }
-txt_find_by_code(){
-
+md_find_by_code(){
+while IFS= read -r line; do
+        awk -F' ' '{print $8}' shell_text_dataset.txt
+    done
 }
-txt_find_by_url(){
-
+md_find_by_url(){
+while IFS= read -r line; do
+        awk -F' ' '{print $9}' shell_text_dataset.txt
+    done
 }
-txt_find_by_tags(){
-
+md_find_by_tags(){
+while IFS= read -r line; do
+        awk -F' ' '{print $10}' shell_text_dataset.txt
+    done
 }
