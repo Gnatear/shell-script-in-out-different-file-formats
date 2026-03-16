c1bf588de7b4378ee5d81f4650be6c79360261de
Gnatear <tzh26144@gmail.com>
2026-03-16 16:48:56 -0300

    function calls from main.sh has been done

--- main.sh
+++ main.sh
@@ -23,25 +23,65 @@ read instruction
 echo "You Entered: $instruction as your Instruction Code."
 
 if [$instruction == 0]; then
-
+csv_find_by_id
+json_find_by_id
+md_find_by_id
+txt_find_by_id
+xlsx_find_by_id
 elif [$instruction == 1]; then
-
+csv_find_by_name
+json_find_by_name
+md_find_by_name
+txt_find_by_name
+xlsx_find_by_name
 elif [$instruction == 2]; then
-
+csv_find_by_email
+json_find_by_email
+md_find_by_email
+txt_find_by_email
+xlsx_find_by_email
 elif [$instruction == 3]; then
-
+csv_find_by_city
+json_find_by_city
+md_find_by_city
+txt_find_by_city
+xlsx_find_by_city
 elif [$instruction == 4]; then
-
+csv_find_by_number
+json_find_by_number
+md_find_by_number
+txt_find_by_number
+xlsx_find_by_number
 elif [$instruction == 5]; then
-
+csv_find_by_unicode
+json_find_by_unicode
+md_find_by_unicode
+txt_find_by_unicode
+xlsx_find_by_unicode
 elif [$instruction == 6]; then
-
+csv_find_by_sentence
+json_find_by_sentence
+md_find_by_sentence
+txt_find_by_sentence
+xlsx_find_by_sentence
 elif [$instruction == 7]; then
-
+csv_find_by_code
+json_find_by_code
+md_find_by_code
+txt_find_by_code
+xlsx_find_by_code
 elif [$instruction == 8]; then
-
+csv_find_by_url
+json_find_by_url
+md_find_by_url
+txt_find_by_url
+xlsx_find_by_url
 elif [$instruction == 9]; then
-
+csv_find_by_tags
+json_find_by_tags
+md_find_by_tags
+txt_find_by_tags
+xlsx_find_by_tags
 else
     echo 'Instruction Code $instruction is not recognizable, please reinsert the Instruction Code:'
     read_instruction_for_search
